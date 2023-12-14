b33e9350f7846c049afc1013eaaf3cecd1f8908e
Jon Andoni Galdos <jonandonigv@gmail.com>
2023-12-14 19:33:42 +0100

    user CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -15,7 +16,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       entities: [],
       synchronize: true,
       autoLoadEntities: true
-    })
+    }),
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
